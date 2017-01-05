ee57799135a437af99590a25389e9e40d5417646
Simon Ekstrand <simon@theoak.se>
2017-01-05 15:51:04 +0100

    Started adding basic support for object model defintions.

---
+++ irisett/object_models.py
@@ -0,0 +1,23 @@
+"""Model/object definitions for commonly used types in Irisett.
+
+This modules uses the attrs module to define models for Irisett objects
+(monitors, alerts, contacts etc).
+
+The model definitions should exactly match the attributes and order of the
+objects in the database.
+"""
+
+# noinspection PyPackageRequirements
+import attr
+# noinspection PyPackageRequirements
+from attr import asdict
+
+
+@attr.s
+class ActiveMonitorAlert:
+    id = attr.ib()
+    monitor_id = attr.ib()
+    start_ts = attr.ib()
+    end_ts = attr.ib()
+    alert_msg = attr.ib()
+    model_type = attr.ib(init=False, default='active_monitor_alert')
