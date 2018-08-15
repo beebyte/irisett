a7208237fcdcfa1af35579154f01e3e429cb58cb
Simon Ekstrand <simon@beebyte.se>
2018-08-15 15:04:51 +0200

    Tweaking pyinstaller.

--- scripts/run-pyinstaller.sh
+++ scripts/run-pyinstaller.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pyinstaller scripts/irisett -n irisett --onefile
+pyinstaller scripts/irisett -n irisett --onefile --add-data 'irisett/webmgmt/static/style.css:irisett/webmgmt/static/' --add-data 'irisett/webmgmt/templates/*:irisett/webmgmt/templates/'
